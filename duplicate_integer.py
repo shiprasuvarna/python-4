6b30f78573a9d6561c64999adb9bb5a82ceb79c0
Shipra Suvarna <102012006+shiprasuvarna@users.noreply.github.com>
2023-04-25 22:33:58 +0530

    Add files via upload

---
+++ duplicate_integer.py
@@ -0,0 +1,8 @@
+#Write a Python program to find out if a given array of integers contains any duplicate elements. Return true if any value appears at least twice in
+#the array, and return false if every element is distinct
+def test_duplicate(array_nums):
+    nums_set = set(array_nums)
+    return len(array_nums) != len(nums_set)
+print(test_duplicate([1,2,3,4,5]))
+print(test_duplicate([1,2,3,4, 4]))
+print(test_duplicate([1,1,2,2,3,3,4,4,5]))
