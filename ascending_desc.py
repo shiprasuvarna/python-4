153753adcca312fb9bba97292726e899b6510002
Shipra Suvarna <shipraasuvarna@gmail.com>
2023-06-23 10:20:20 +0530

    Update ascending_desc.py

--- ascending_desc.py
+++ ascending_desc.py
@@ -1,4 +1,4 @@
-#Write a Python script to sort (ascending and descending) a dictionary by value
+#Write a Python script to sort (ascending and descending) a dictionary by value.
 import operator
 d = {1: 2, 3: 4, 4: 3, 2: 1, 0: 0}
 print('Original dictionary : ',d)
