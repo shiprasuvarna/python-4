47203ab1aa8e0ab80f824f9c3197791df1bfce2a
Shipra Suvarna <shipraasuvarna@gmail.com>
2023-06-27 10:34:44 +0530

    Update barchart_popularity.py

--- bar graph/barchart_popularity.py
+++ bar graph/barchart_popularity.py
@@ -2,6 +2,7 @@
 #Programming languages: Java, Python, PHP, JavaScript, C#, C++
 #Popularity: 22.2, 17.6, 8.8, 8, 7.7, 6.7
 #Use different color for each bar.
+
 import matplotlib.pyplot as plt
 x = ['Java', 'Python', 'PHP', 'JavaScript', 'C#', 'C++']
 popularity = [22.2, 17.6, 8.8, 8, 7.7, 6.7]
